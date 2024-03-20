5a010c9ecd9932357c3319368bae0e2f800b6e0b
vthe <idashatao@gmail.com>
2024-03-20 18:50:41 +0800

    Global Rwlock And Value Mutex

--- homework/src/hello_server/cache.rs
+++ homework/src/hello_server/cache.rs
@@ -4,13 +4,15 @@ use std::collections::hash_map::{Entry, HashMap};
 use std::hash::Hash;
 use std::sync::{Arc, Mutex, RwLock};
 
+type Inner<T> = Arc<Mutex<Option<T>>>;
+
 /// Cache that remembers the result for each key.
 #[derive(Debug, Default)]
 pub struct Cache<K, V> {
     // todo! This is an example cache type. Build your own cache type that satisfies the
     // specification for `get_or_insert_with`.
     // inner: Mutex<HashMap<K, V>>,
-    inner: Arc<RwLock<HashMap<K, Arc<RwLock<Option<V>>>>>>,
+    inner: Arc<RwLock<HashMap<K, Inner<V>>>>,
 }
 
 // impl<K, V> Default for Cache<K, V> {
@@ -37,45 +39,26 @@ impl<K: Eq + Hash + Clone, V: Clone> Cache<K, V> {
     ///
     /// [`Entry`]: https://doc.rust-lang.org/stable/std/collections/hash_map/struct.HashMap.html#method.entry
     pub fn get_or_insert_with<F: FnOnce(K) -> V>(&self, key: K, f: F) -> V {
-        // let value = Arc::new(RwLock::new(None));
-        // let mut write_lock = self.inner.write().unwrap();
+        let value = Arc::new(Mutex::new(None));
 
-        // if !write_lock.contains_key(&key) {
-        //     write_lock.insert(key.clone(), Arc::clone(&value));
-        // }
-
-        // let stored_value = write_lock.get(&key).unwrap().clone();
-        // drop(write_lock);
-
-        // {
-        //     let r = stored_value.read().unwrap();
-        //     if r.is_none() {
-        //         drop(r);
-        //         let v = f(key);
-        //         let mut w = stored_value.write().unwrap();
-        //         *w = Some(v.clone());
-        //         return v.clone();
-        //     } else {
-        //         r.clone().unwrap()
-        //     }
-
-        //     // stored_value.read().unwrap().unwrap()
-        // }
         let mut write_lock = self.inner.write().unwrap();
-        match write_lock.entry(key.clone()) {
-            Entry::Occupied(entry) => {
-                let stored_value = entry.get().clone();
-                let read_guard = stored_value.read().unwrap();
-                read_guard.clone().unwrap()
-            }
-            Entry::Vacant(entry) => {
-                let value = Arc::new(RwLock::new(None));
-                entry.insert(value.clone());
-                drop(write_lock);
-                let returned_value = f(key);
-                let mut write_guard = value.write().unwrap();
-                *write_guard = Some(returned_value.clone());
-                returned_value
+
+        if !write_lock.contains_key(&key) {
+            write_lock.insert(key.clone(), Arc::clone(&value));
+        }
+
+        let stored_value = Arc::clone(&write_lock.get(&key).unwrap());
+
+        drop(write_lock);
+
+        {
+            let mut lock = stored_value.lock().unwrap();
+            if lock.is_none() {
+                let v = f(key.clone());
+                *lock = Some(v.clone());
+                v.clone()
+            } else {
+                lock.clone().unwrap()
             }
         }
     }
